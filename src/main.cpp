33dff7e2a60fbd34966eade54264126f75183a11
maanasa-surneni <msurneni@gmail.com>
2026-07-11 16:26:39 -0400

    store generated telemetry messages

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,33 @@
 #include <iostream>
-#include "TelemetryMessage.hpp"
+#include <vector>
+
 #include "SensorSimulator.hpp"
 
 int main()
 {
     SensorSimulator simulator("sensor-01");
 
-    for (int i{0}; i<5; ++i){
+    //creates an initially empty collection that can hold many TelemetryMessage objects
+    std::vector<TelemetryMessage> messages;
+
+    for (int i{0}; i<10; ++i){
         TelemetryMessage message = simulator.generateMessage();
 
-        message.print();
-
         if (message.isValid()){
-            std::cout << "Message is valid." << std::endl;
+            //the push_back adds a message to the end of the vector
+            messages.push_back(message);
         } else {
-            std::cout << "Message is invalid." << std::endl;
+            std::cout << "Invalid message rejected." << std::endl;
         }
-        std::cout << std::endl;
     }
 
+    std::cout << "Store messages: " << messages.size() << std::endl;
+
+    //loops through every message in the vector
+    for (const TelemetryMessage& message : messages){
+        message.print();
+        std::cout << std::endl;
+    }
 
     return 0;
 
